4e201c2a78187737343a9506928ae5a64d8fd904
xxxx <squall0609.kanon@gmail.com>
2021-02-21 17:40:41 +0900

    vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,21 @@
 <template>
   <v-container>
-    <v-row class="text-center">
+    <v-row class="text-center" justify="center">
+      <v-col class="mb-20" justify="center">
+        <v-container class="ml-50">
+          <div class="ml-50">
+            <v-layout wrap class="ml-50" justify="center">
+              <v-icon large color="blue darken-2" class="ml-50">
+                mdi-message-text
+              </v-icon>
 
-      <v-col class="mb-20">
-        <v-layout wrapper>
-          <v-icon
-            large
-            color="blue darken-2"
-            class="mr-3"
-          >
-            mdi-message-text
-          </v-icon>
-          <h2 class="title font-weight-bold mb-3">
-            死ぬまでにやりたいことリスト <br />
-          </h2>
-        </v-layout>
+              <h2 class="title font-weight-bold mb-3">
+                死ぬまでにやりたいことリスト <br />
+              </h2>
+            </v-layout>
+          </div>
+        </v-container>
+
         <h2 class="title font-weight-bold mb-3">
           ~List of things to do before you die~
         </h2>
@@ -27,8 +28,18 @@
             <v-row>
               <v-col cols="12" sm="12">
                 <v-layout wrap>
-                  <v-text-field v-model="newTodoName" solo placeholder="やりたいこと"></v-text-field>
-                  <v-btn @click="addTodo()" class="mx-2" fab dark color="indigo">
+                  <v-text-field
+                    v-model="newTodoName"
+                    solo
+                    placeholder="やりたいこと"
+                  ></v-text-field>
+                  <v-btn
+                    @click="addTodo()"
+                    class="mx-2"
+                    fab
+                    dark
+                    color="indigo"
+                  >
                     <v-icon dark>
                       mdi-plus
                     </v-icon>
@@ -41,16 +52,17 @@
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <v-row justify="center" v-for="(todo,key) in todos" :key="key">
+        <v-row justify="center" v-for="(todo, key) in todos" :key="key">
           <v-layout wrapper>
-            <input 
+            <input
               type="checkbox"
               id="target"
-              v-model="todo.isComplete" 
+              v-model="todo.isComplete"
               @click="updateTodo(todo, key)"
-              style="transform:scale(2.0);" 
-              class="mx-5" 
-              text-size="16px"/>
+              style="transform:scale(2.0);"
+              class="mx-5"
+              text-size="16px"
+            />
             {{ todo.name }}
             <v-btn
               @click="removeTodo(key)"
@@ -66,7 +78,6 @@
               </v-icon>
             </v-btn>
           </v-layout>
-
         </v-row>
       </v-col>
     </v-row>
@@ -74,54 +85,58 @@
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
 
 export default {
   name: "HelloWorld",
-  created(){
-    this.db = firebase.firestore()
-    this.todosRef = this.db.collection('todos')
+  created() {
+    this.db = firebase.firestore();
+    this.todosRef = this.db.collection("todos");
     //todoのデータが変わったとき（追加、削除など）、リスナーを登録して、処理を行う
-    this.todosRef.onSnapshot(querySnapshot => { //querySnapshot=現在の全てのデータ
-      const obj = {}
-      querySnapshot.forEach(doc => { //doc=firebaseのデータ内のドキュメント
+    this.todosRef.onSnapshot((querySnapshot) => {
+      //querySnapshot=現在の全てのデータ
+      const obj = {};
+      querySnapshot.forEach((doc) => {
+        //doc=firebaseのデータ内のドキュメント
         //doc.id=ドキュメントのID doc.data=コレクション
-        obj[doc.id] = doc.data()
-      })
-      this.todos = obj
-    })
+        obj[doc.id] = doc.data();
+      });
+      this.todos = obj;
+    });
   },
 
   data: () => ({
-   db :null,
-   todosRef: null,
-   newTodoName: '',
-   todos: {},
+    db: null,
+    todosRef: null,
+    newTodoName: "",
+    todos: {},
   }),
   methods: {
-    addTodo(){
-      if(this.newTodoName === ''){ return }
+    addTodo() {
+      if (this.newTodoName === "") {
+        return;
+      }
       this.todosRef.add({
         name: this.newTodoName,
         isComplete: false,
       }),
-      this.newTodoName = ''
+        (this.newTodoName = "");
     },
-    updateTodo(todo, key){
-      todo.isComplete = !todo.isComplete
-      this.todosRef.doc(key).update(todo)
-      
+    updateTodo(todo, key) {
+      todo.isComplete = !todo.isComplete;
+      this.todosRef.doc(key).update(todo);
+
       var id = document.getElementById(todo);
-        if(todo.isComplete == true){
-          id.style.textDecoration = "line-through";
-        }else{
-         id.style.textDecoration = "none";
-        }
+      if (todo.isComplete == true) {
+        id.style.textDecoration = "line-through";
+      } else {
+        id.style.textDecoration = "none";
+      }
     },
-    removeTodo(key){
-      this.todosRef.doc(key).delete()
-    } 
-  }
+    removeTodo(key) {
+      this.todosRef.doc(key).delete();
+    },
+  },
 };
 </script>
 
@@ -130,7 +145,7 @@ export default {
   font-family: "M Plus 1p" !important;
 }
 
-  .v-text-field input {
-    font-size: 1em;
-  }
+.v-text-field input {
+  font-size: 1em;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,25 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import firebase from 'firebase'
+import router from './router'
 
 Vue.config.productionTip = false
 
- // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyBQ_QL7Xj9kHr8AUJU-4k2hEzmKmGxZftI",
-    authDomain: "want-do.firebaseapp.com",
-    projectId: "want-do",
-    storageBucket: "want-do.appspot.com",
-    messagingSenderId: "118442981768",
-    appId: "1:118442981768:web:7184f5c68727855397e23d",
-    measurementId: "G-LM5MM0HH8Y"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyBQ_QL7Xj9kHr8AUJU-4k2hEzmKmGxZftI",
+  authDomain: "want-do.firebaseapp.com",
+  projectId: "want-do",
+  storageBucket: "want-do.appspot.com",
+  messagingSenderId: "118442981768",
+  appId: "1:118442981768:web:7184f5c68727855397e23d",
+  measurementId: "G-LM5MM0HH8Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
